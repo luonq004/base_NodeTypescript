e981939fc5204b66c6579e72e94e24df244ab05d
Luong <luongvq004@gmail.com>
2024-11-29 23:59:12 +0700

    create base

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
+
 import { userRouter } from "./users/user.router";
 
 const app = express();
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
